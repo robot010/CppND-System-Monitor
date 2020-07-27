fd16cb2c0f786db802d0846b74b48da7a8ed30a5
Bato's Udacity workspace <dengbotaoo@gmail.com>
2020-07-27 23:31:47 +0000

    Add time format conversion function.

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,28 @@
 #include <string>
+#include <cmath>
+#include <sstream>
 
 #include "format.h"
 
 using std::string;
+using std::floor; 
+using std::stringstream;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) 
+{ 
+    int hours, mins, secs; 
+    stringstream ss; 
+
+    hours = floor(seconds/3600);
+    mins = floor(seconds % 3600 / 60);
+    secs = seconds - hours*3600 - mins*60;
+
+    ss << hours << ":" << mins << ":" << secs;
+
+    return ss.str();
+   
+}
